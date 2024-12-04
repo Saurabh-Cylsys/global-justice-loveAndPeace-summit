36bcd8f389fb8d023c194b1438f1571f92c73945
Kumar Saurabh <saurabh.kumar@cylsys.com>
2024-12-04 17:43:05 +0530

    change partners logos

--- src/app/Home/web/partners/partners.component.ts
+++ src/app/Home/web/partners/partners.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-partners',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./partners.component.css']
 })
 export class PartnersComponent {
+  isMobileView = false;
+  // headerIcon:any
+  constructor( private SharedService: SharedService
+  ) {}
+
+  ngOnInit(): void {
+    this.checkWindowSize();
+
+  }
+  checkWindowSize(): void {
+    if (window.innerWidth <= 767) {
+      this.SharedService.isMobileView.next(true);
+      this.isMobileView = true;
+    } else {
+      this.SharedService.isMobileView.next(false);
+      this.isMobileView = false;
+    }
+  }
+
+  // Listen to window resize events
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any): void {
+    this.checkWindowSize();
+  }
 
 }
