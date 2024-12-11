0d3a360a1218352e4d50c97c9e0cf6bdc5694400
nadeem <nadeem@titan-consultancy.com>
2024-12-11 14:14:32 +0530

    fixed bug on dalegate page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,9 @@
 export const environment = {
   production: true,
 
-  // apiUrl: 'https://apis.pharmapreconnectcongress.com/api/v1',   //production
+  // apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
   apiUrl : 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
-  // apiUrl: 'http://localhost:5000/api/v1',  //local
   apiMockUrl:'/api/v1',
   domainUrl:'http://localhost:5000/',
-
-  // apiUrl:location.origin,
-  // apiMockUrl:location.origin,
-  // domainUrl:location.origin,
   app_env: 'production'
 };
