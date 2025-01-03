8603ffbc52fd74102e66eae47a59e3e1366da1c9
Kumar Saurabh <saurabhkumar350@gmail.com>
2025-01-03 09:12:17 +0530

    change ui for chairman corner page

--- src/app/Home/web/chairman-corner/chairman-corner.component.ts
+++ src/app/Home/web/chairman-corner/chairman-corner.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {ActivatedRoute } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import {ActivatedRoute, Router } from '@angular/router';
+import { SharedService } from 'src/app/shared/services/shared.service';
 
 @Component({
   selector: 'app-chairman-corner',
@@ -7,8 +8,15 @@ import {ActivatedRoute } from '@angular/router';
   styleUrls: ['./chairman-corner.component.css']
 })
 export class ChairmanCornerComponent {
-  constructor( private router: ActivatedRoute
+  isMobileView = false;
+  constructor( private router: ActivatedRoute,
+    public _router: Router,
+        private SharedService: SharedService
   ) {}
+
+  ngOnInit(): void {
+    this.checkWindowSize();
+  }
   
   ngAfterViewInit(): void {
     this.router.fragment.subscribe((fragment) => {
@@ -20,4 +28,20 @@ export class ChairmanCornerComponent {
       }
     });
   }
+
+  checkWindowSize(): void {
+      if (window.innerWidth <= 767) {
+        this.SharedService.isMobileView.next(true);
+        this.isMobileView = true;
+      } else {
+        this.SharedService.isMobileView.next(false);
+        this.isMobileView = false;
+      }
+    }
+  
+    // Listen to window resize events
+    @HostListener('window:resize', ['$event'])
+    onResize(event: any): void {
+      this.checkWindowSize();
+    }
 }
