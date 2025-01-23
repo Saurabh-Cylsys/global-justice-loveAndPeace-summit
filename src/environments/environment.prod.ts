017161f0ef7970ce10d7c7f5f59b1d8c57278d1b
Kumar Saurabh <saurabhkumar350@gmail.com>
2025-01-24 01:56:08 +0530

    development server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
 
-  apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
-  domainUrl:'https://www.justice-love-peace.com',//production
-  // apiUrl : 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
-  // domainUrl:'https://globaljusticeuat.cylsys.com',//development
+  // apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
+  // domainUrl:'https://www.justice-love-peace.com',//production
+  apiUrl : 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
+  domainUrl:'https://globaljusticeuat.cylsys.com',//development
   apiMockUrl:'/api/v1',
   app_env: 'production'
 };
