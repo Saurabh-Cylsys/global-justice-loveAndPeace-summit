8e3e2cac4752dd0471646e73726f2e2f87325265
nadeem <nadeem@titan-consultancy.com>
2025-02-04 17:18:58 +0530

    add api-enviroment for QA

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,10 @@ export const environment = {
 
   // apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
   // domainUrl:'https://www.justice-love-peace.com',
+
+  // apiUrl : 'https://globaljusticeqaapi.cylsys.com/api/v1',   //QA
+  // domainUrl:'https://globaljusticeqa.cylsys.com',
+
   apiUrl : 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
   domainUrl:'https://globaljusticeuat.cylsys.com',
   apiMockUrl: '/api/v1',
