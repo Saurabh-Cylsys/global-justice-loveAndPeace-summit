0d5a117424259b24e20b88b8109b9b0545fd012b
rupeshnarkar92 <rupesh.narkar@cylsys.com>
2025-02-08 09:40:02 +0530

    live environment updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
 
-  // apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
+  apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
   // domainUrl:'https://www.justice-love-peace.com',//production
   // apiUrl : 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
 
-  apiUrl : 'https://globaljusticeqcapi.cylsys.com/api/v1' ,       //QC
+  // apiUrl : 'https://globaljusticeqcapi.cylsys.com/api/v1' ,       //QC
   domainUrl:'https://globaljusticeuat.cylsys.com',//development
   apiMockUrl:'/api/v1',
   app_env: 'production'
