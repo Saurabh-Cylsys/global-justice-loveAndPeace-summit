e1c7b778721ed7e82e6fc054486af9298b738ce9
rupeshnarkar92 <rupesh.narkar@cylsys.com>
2025-02-08 10:42:09 +0530

    environment updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,11 @@ export const environment = {
   production: true,
 
   apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
-  // domainUrl:'https://www.justice-love-peace.com',//production
+  domainUrl:'https://www.justice-love-peace.com',////production for peackeeper qrcode
   // apiUrl : 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
 
   // apiUrl : 'https://globaljusticeqcapi.cylsys.com/api/v1' ,       //QC
-  domainUrl:'https://globaljusticeuat.cylsys.com',//development
+  // domainUrl:'https://globaljusticeuat.cylsys.com',//development
   apiMockUrl:'/api/v1',
   app_env: 'production'
 };
