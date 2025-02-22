f60d49a391f1ef114afbba39019708603c6edaa3
Nadeem Nakade <nadeem@titan-consultancy.com>
2025-02-22 09:20:07 +0530

    page now loading from static html

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
+if (window.location.pathname === "/") {
+  window.location.href = "/assets/static/index.html";
+} else {
+ 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+}
