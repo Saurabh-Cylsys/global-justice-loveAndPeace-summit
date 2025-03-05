1c43ad31f0af6f30e81f5fdaabf0daa551f9ab68
Nadeem Nakade <nadeem@titan-consultancy.com>
2025-03-05 16:59:33 +0530

    payment-success page added

---
+++ src/app/features/delegate/components/delegate-payment-success/delegate-payment-success.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delegate-payment-success',
+  templateUrl: './delegate-payment-success.component.html',
+  styleUrls: ['./delegate-payment-success.component.css']
+})
+export class DelegatePaymentSuccessComponent {
+
+}
--- src/app/features/delegate/delegate.module.ts
+++ src/app/features/delegate/delegate.module.ts
@@ -14,6 +14,7 @@ import { DelegateRegistrationSeoComponent } from './components/delegate-registra
 import { DelegateRegistrationOnlineComponent } from './components/delegate-registration-online/delegate-registration-online.component';
 import { DelegatePeaceStudentComponent } from './components/delegate-peace-student/delegate-peace-student.component';
 import { DelegateMessageComponent } from './components/delegate-message/delegate-message.component';
+import { DelegatePaymentSuccessComponent } from './components/delegate-payment-success/delegate-payment-success.component';
 @NgModule({
   declarations: [
     DelegateMainComponent,
@@ -23,7 +24,8 @@ import { DelegateMessageComponent } from './components/delegate-message/delegate
     DelegateRegistrationSeoComponent,
     DelegateRegistrationOnlineComponent,
     DelegatePeaceStudentComponent,
-    DelegateMessageComponent
+    DelegateMessageComponent,
+    DelegatePaymentSuccessComponent
   ],
   imports: [
     CommonModule,
