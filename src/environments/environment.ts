1137041354d86a58d780d37e2b3b735ef40ba39a
Nadeem Nakade <Nadeem@titan-consultancy.com>
2025-03-14 16:02:05 +0530

    comit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,11 @@ export const environment = {
 
   apiUrl : 'https://devglobaljusticeapis.cylsys.com/api/v1',   //development
   domainUrl:'https://globaljusticeuat.cylsys.com',           //development for peackeeper qrcode
-  tinyUrl :'https://tinyurl.com/5n8txwht',          //tiny url for mediator page
+  //tinyUrl :'https://tinyurl.com/5n8txwht',          //tiny url for mediator page
 
   //apiUrl : 'https://globaljusticeqcapi.cylsys.com/api/v1' ,       //QC
   // domainUrl:'https://globaljusticeqc.cylsys.com',         //QC for peackeeper qrcode
-  // tinyUrl : 'https://tinyurl.com/7yuvnmyj',               //tiny url for mediator page
+  tinyUrl : 'https://tinyurl.com/7yuvnmyj',               //tiny url for mediator page
 
   // tinyUrl : 'https://tinyurl.com/3322sj49',   // For Local
 
