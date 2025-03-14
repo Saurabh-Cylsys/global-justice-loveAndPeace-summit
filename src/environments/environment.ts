0067aff394bd86e2264877e9e02a080b8205d780
Nadeem Nakade <Nadeem@titan-consultancy.com>
2025-03-14 23:27:10 +0530

    coupon code validation integrated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,9 +6,10 @@ export const environment = {
   domainUrl:'https://globaljusticeuat.cylsys.com',           //development for peackeeper qrcode
   //tinyUrl :'https://tinyurl.com/5n8txwht',          //tiny url for mediator page
 
+  tinyUrl :'https://tinyurl.com/7yuvnmyj',
   //apiUrl : 'https://globaljusticeqcapi.cylsys.com/api/v1' ,       //QC
   // domainUrl:'https://globaljusticeqc.cylsys.com',         //QC for peackeeper qrcode
-  tinyUrl : 'https://tinyurl.com/7yuvnmyj',               //tiny url for mediator page
+  //tinyUrl : 'https://tinyurl.com/7yuvnmyj',               //tiny url for mediator page
 
   apiMockUrl: '/api/v1',
   app_env: 'development',
