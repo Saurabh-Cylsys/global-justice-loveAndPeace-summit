c02e55bce722339613197fddcf2031e534beaf5e
Kumar Saurabh <saurabhkumar350@gmail.com>
2025-07-02 15:49:41 +0530

    change api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
 
-  apiUrl: 'https://api.justice-love-peace.com/api/v1',   //production
-  domainUrl: 'https://www.justice-love-peace.com',////production for peackeeper qrcode
+  apiUrl: 'https://gjlp2025api.cylsysuat.com/api/v1',   //production
+  domainUrl: 'https://pre-live-gjlp.cylsysuat.com',////production for peackeeper qrcode
   tinyUrl: 'https://tinyurl.com/ys5z7n2z',         //tiny url for mediator page
   apiMockUrl: '/api/v1',
   app_env: 'production',
